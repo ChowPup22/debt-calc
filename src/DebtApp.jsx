fb0bbbb223e4b0bfc7836e355fa3b29934733b08
ChowPup22 <97262944+ChowPup22@users.noreply.github.com>
2022-09-04 15:22:31 -0400

    Update Debt App
    add logic for min payments, and 'final payment', add general css and clean up some of the working code

--- src/DebtApp.jsx
+++ src/DebtApp.jsx
@@ -8,62 +8,108 @@ class DebtCalcApp extends React.Component {
       totalDebt: 0,
       interest: 0,
       minPay: 0,
+      numPayments: 0,
       debtInfo: [],
       payNow: 0,
       remDebt: 0,
       date: '',
       id: '',
       payHist: [],
+      read: false,
     }
   }
 
   handleDebt = ({target: { value }}) => this.setState({totalDebt: value});
-  handleInt = ({target: { value }}) => this.setState({interest: value}, this.handleMinPay());
+  handleInt = ({target: { value }}) => this.setState({interest: value}, this.handleMinPay(value));
   handlePayNow = ({target: {value}}) => this.setState({ payNow: value});
 
-  handleMinPay = () => {
-    const {totalDebt, interest} = this.state;
+  handleMinPay = (value) => {
 
-    const int = ((interest / 100) /12 * totalDebt);
+    const {totalDebt} = this.state;
 
-    const min = ((totalDebt * 0.01) + int).toFixed(2);
-  
-    this.setState({minPay: min});
+    if (totalDebt > 100) {
+      const rate = value /100;
 
-    const newInfo = {
-      intNow: int.toFixed(2),
-      totalDebt: totalDebt,
+      const int = (rate * totalDebt)/12;
+
+      const min = ((totalDebt * 0.01) + int).toFixed(2);
+
+      const num = (totalDebt /min).toFixed(0);
+    
+      const newInfo = {
+        intNow: int.toFixed(2),
+        totalDebt: totalDebt,
+      }
+
+      this.setState({minPay: min, debtInfo: newInfo, numPayments: num});
+
+    } else if (totalDebt <= 100) {
+      const min = ((totalDebt * 0.01) + totalDebt).toFixed(2);
+
+      const num = (totalDebt /min).toFixed(0);
+
+      const newInfo = {
+        intNow: 0,
+        totalDebt: totalDebt,
+      }
+
+      this.setState({minPay: min, debtInfo: newInfo, numPayments: num});
     }
 
-    this.setState({debtInfo: newInfo});
+    
+    // this.setState({numPayments: num.toFixed(0)});
     
   };
 
   handleSubmit = (e) => {
+    const {debtInfo, payNow, minPay, interest} = this.state;
     e.preventDefault();
 
-    const {debtInfo, payNow} = this.state;
+    if (payNow >= minPay) {
 
-    const rem = (debtInfo.totalDebt - (payNow - debtInfo.intNow)).toFixed(2);
-    
+      const principle = (payNow - debtInfo.intNow);
 
-    const newItem = {
-      payNow: payNow,
-      remDebt: rem,
-      date: new Date(),
-      id: Date.now(),
+      const rem = (debtInfo.totalDebt - principle).toFixed(2);
+      
+      const timeStamp = () => {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+        var hrs = today.getHours();
+        var mins = today.getMinutes();
+        var sec = today.getSeconds();
+        
+        if(dd<10) dd='0'+dd;
+        if(mm<10) mm='0'+mm;
+        if(hrs<10) hrs='0'+hrs;
+        if(mins<10) mins='0'+mins;
+        if(sec<10) sec='0'+sec;
+        return (mm+'-'+dd+'-'+yyyy+' :: '+hrs+':'+mins+':'+sec);
+        };
+
+      const newItem = {
+        payNow: payNow,
+        remDebt: rem,
+        date: timeStamp(),
+        id: Date.now(),
+      }
+
+      this.setState((state) => ({ 
+        payHist: [...state.payHist, newItem],
+        remDebt: rem,
+        totalDebt: rem,
+        payNow: 0,
+        date: '',
+        id: '',
+        read: true,
+      }));
+      
+    } else if (payNow < minPay) {
+      this.setState({payNow: 0})
+      alert(`Payment must be greater than or equal to the Minimum Payment! ($${minPay})`);
     }
-
-    this.setState((state) => ({ 
-      payHist: [...state.payHist, newItem],
-      remDebt: rem,
-      totalDebt: rem,
-      payNow: 0,
-      date: '',
-      id: '',
-    }));
-
-    this.handleMinPay();
+    this.handleMinPay(interest);
   }
 
   render() {
@@ -80,6 +126,7 @@ class DebtCalcApp extends React.Component {
             type="number"
             autoComplete='off'
             value={this.state.totalDebt}
+            readOnly={this.state.read}
           />
           <br />
           <label htmlFor="interest">Interest Rate</label>
@@ -89,6 +136,7 @@ class DebtCalcApp extends React.Component {
             type="number" 
             autoComplete='off'
             value={this.state.interest}
+            readOnly={this.state.read}
           />
           <br />
           <br />
@@ -100,6 +148,7 @@ class DebtCalcApp extends React.Component {
               value={this.state.totalDebt}
               readOnly={true}
             />
+            <h5>Estimated Minimum Payments: {this.state.numPayments}</h5>
             <h4>Min Payment: ${this.state.minPay}</h4>
           </div>
           <label htmlFor="payNow">Enter Payment Amount</label>
--- src/DebtHist.jsx
+++ src/DebtHist.jsx
@@ -5,11 +5,15 @@ class DebtHist extends React.Component {
   render() {
     const {items} = this.props;
     return (
-      <ul>
-        {items.forEach((item) => (
-          <li key={item.id}>Date: {item.date}  Amount: {item.payNow}  Remaining: {item.remDebt}</li>
+      <div>
+        <h3>Transaction History</h3>
+        <hr />
+        <ul className="debt-hist">
+        {items.map((item) => (
+          <li key={item.id}>Date:{item.date} ~~ Amount: ${item.payNow} ~~ Remaining: ${item.remDebt}</li>
         ))}
       </ul>
+      </div>
     )
   }
 }
